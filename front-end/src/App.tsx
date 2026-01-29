f114c52542322de9e47e716787018da4bb4e9ea0
faqih28alam <faqih28alam@gmail.com>
2026-01-29 10:26:02 +0700

    Setup Front-End

---
+++ front-end/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>AI Journal App</h1>
+      <h1>Front End Ready</h1>
+    </div>
+  )
+}
+
+export default App
